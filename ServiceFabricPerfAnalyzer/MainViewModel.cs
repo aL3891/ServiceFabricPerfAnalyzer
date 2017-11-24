632c9d5d99bc8b22e9e222f77fcb6c27f15b558e
Allan Lindqvist <aL_3891@hotmail.com>
2017-11-24 21:52:09 +0100

    no message

---
+++ ServiceFabricPerfAnalyzer/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using ObservableDatabinding;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFabricPerfAnalyzer
+{
+	public class MainViewModel
+	{
+		public XamlSubject<PerformanceCounterCategory[]> Categories { get; set; } = new XamlSubject<PerformanceCounterCategory[]>();
+
+		public XamlSubject<PerformanceCounterCategory> SelectedCategory { get; set; } = new XamlSubject<PerformanceCounterCategory>();
+
+
+
+		public XamlObservable<PerfViewModel[]> PerfValues { get; set; } = new XamlObservable<PerfViewModel[]>();
+
+		public MainViewModel()
+		{
+			var cats = PerformanceCounterCategory.GetCategories();
+			var sfActors = cats.FirstOrDefault(c => c.CategoryName == "Service Fabric Actor");
+			var sfActorMethods = cats.FirstOrDefault(c => c.CategoryName == "Service Fabric Actor Method");
+			var sfServices = cats.FirstOrDefault(c => c.CategoryName == "Service Fabric Service");
+			var sfServiceMethod = cats.FirstOrDefault(c => c.CategoryName == "Service Fabric Service Method");
+
+			Categories.Value = cats;
+
+			SelectedCategory.Observable.CombineLatest(Observable.Interval(TimeSpan.FromSeconds(5)), (p, i) => p);
+
+
+			PerfValues.Observable = SelectedCategory.Observable.Select(prop =>
+			{
+				var values = prop.ReadCategory();
+				
+
+
+				return new PerfViewModel[0];
+
+			});
+		}
+	}
+
+	public class PerfViewModel
+	{
+
+	}
+}
